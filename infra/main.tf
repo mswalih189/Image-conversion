c887ff80ea1c0ffad7794872f050a124c89fb90e
swali <swalihm189@gmail.com>
2025-12-23 17:58:07 +0530

    Modify user_data for Flask app setup on EC2
    Updated user_data to include installation of Flask app and systemd service configuration.

--- infra/main.tf
+++ infra/main.tf
@@ -22,8 +22,40 @@ resource "aws_instance" "proj4_ec2" {
     Name = "proj4-ec2"
   }
 
-  user_data = filebase64("../user_data.sh")
-}
+user_data = base64encode(<<-EOF
+#!/bin/bash
+yum update -y
+yum install -y python3 python3-pip git
+pip3 install flask boto3 pillow gunicorn
+
+mkdir -p /opt/image_s3_app
+cd /opt/image_s3_app
+git clone https://github.com/mswalih189/Image-conversion.git .
+pip3 install -r requirements.txt
+
+cat > /etc/systemd/system/flask-app.service <<SERVICE
+[Unit]
+Description=Flask Image Conversion App
+After=network.target
+
+[Service]
+User=ec2-user
+WorkingDirectory=/opt/image_s3_app
+Environment="FLASK_APP=app.py"
+Environment="FLASK_ENV=production"
+ExecStart=/usr/local/bin/gunicorn --bind 0.0.0.0:5000 app:app
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+SERVICE
+
+systemctl daemon-reload
+systemctl enable flask-app
+systemctl start flask-app
+EOF
+)
+
 
 resource "aws_s3_bucket" "proj4_bucket" {
   bucket = "proj4-image-db-${random_id.suffix.hex}"
